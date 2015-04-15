e8aa19a0b1fa6b2867e98b4935e4e9da8eac05d2
zhouxinan <zhouxinan@qq.com>
2015-04-15 18:06:52 +0800

    implemented getCurrentTime

--- Lab6/lab6.js
+++ Lab6/lab6.js
@@ -16,10 +16,41 @@ window.onresize = function(){
 }
 
 function countWords() {
-	var len = text.value.length;
+	var remain = 140 - text.value.length;
 	var promote = document.getElementById("promote");
-	promote.textContent = 140 - len;
+	promote.textContent = remain;
+	return remain;
+}
+
+function tweet() {
+	if (countWords() >= 0) {
+		alert("tweet!");
+		text.value = "";
+		countWords();
+	} else alert("naive!");
+}
+
+function getCurrentTime() {
+	var currentTime = new Date();
+	var day = currentTime.getDate();
+	var month = currentTime.getMonth() + 1;
+	var year = currentTime.getFullYear();
+	var hours = currentTime.getHours();
+	var minutes = currentTime.getMinutes();
+	var currentTimeString = year + "/" + month + "/" + day + " ";
+	if (minutes < 10){
+		minutes = "0" + minutes;
+	}
+	currentTimeString += hours + ":" + minutes + " ";
+	if (hours > 11){
+		currentTimeString += "PM";
+	} else {
+		currentTimeString += "AM";
+	}
+	return currentTimeString;
 }
 
 text = document.getElementById("text");
 text.onkeyup = countWords;
+submit = document.getElementById("submit");
+submit.onclick = tweet;
