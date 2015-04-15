675ba933809a89ee0c0a88107aa64cc044452209
zhouxinan <zhouxinan@qq.com>
2015-04-15 17:35:36 +0800

    implemented words counting

--- Lab6/lab6.js
+++ Lab6/lab6.js
@@ -14,3 +14,12 @@ window.onresize = function(){
 	var write = document.getElementById("write");
 	write.style.left = (window.innerWidth - 900)/2 + "px";
 }
+
+function countWords() {
+	var len = text.value.length;
+	var promote = document.getElementById("promote");
+	promote.textContent = 140 - len;
+}
+
+text = document.getElementById("text");
+text.onkeyup = countWords;
