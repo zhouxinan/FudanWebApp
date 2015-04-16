27ee4d8a5c51fdc11606c980c89ea8c0a727cd87
zhouxinan <zhouxinan@qq.com>
2015-04-16 21:24:19 +0800

    implemented Part One

--- Lab6/lab6.js
+++ Lab6/lab6.js
@@ -17,17 +17,51 @@ window.onresize = function(){
 
 function countWords() {
 	var remain = 140 - text.value.length;
-	var promote = document.getElementById("promote");
-	promote.textContent = remain;
+	document.getElementById("promote").textContent = remain;
 	return remain;
 }
 
 function tweet() {
 	if (countWords() >= 0) {
-		alert("tweet!");
+		var newTweet = document.createElement("div");
+		newTweet.className = "tweet";
+		var quote = document.createElement("div");
+		quote.className = "quote";
+		var username = document.createElement("span");
+		username.className = "username";
+		username.appendChild(document.createTextNode("江泽民"));
+		var userImage = document.createElement("img");
+		userImage.className = "userImage";
+		userImage.src = "img/user.jpg";
+		var tweetContent = document.createElement("span");
+		tweetContent.className = "tweetContent";
+		tweetContent.appendChild(document.createTextNode(text.value));
+		var tweetInfo = document.createElement("div");
+		tweetInfo.className = "tweetInfo";
+		var tweetTime = document.createElement("span");
+		tweetTime.className = "tweetTime";
+		tweetTime.appendChild(document.createTextNode(getCurrentTime()));
+		var from = document.createElement("span");
+		from.className = "from";
+		from.appendChild(document.createTextNode("来自网页"));
+		var forward = document.createElement("span");
+		forward.className = "forward";
+		forward.appendChild(document.createTextNode("转发"));
+		var clear = document.createElement("div");
+		clear.className = "clear";
+		quote.appendChild(username);
+		quote.appendChild(tweetContent);
+		tweetInfo.appendChild(tweetTime);
+		tweetInfo.appendChild(from);
+		tweetInfo.appendChild(forward);
+		newTweet.appendChild(userImage);
+		newTweet.appendChild(quote);
+		newTweet.appendChild(tweetInfo);
+		newTweet.appendChild(clear);
+		document.getElementById("display").appendChild(newTweet);
 		text.value = "";
 		countWords();
-	} else alert("naive!");
+	}
 }
 
 function getCurrentTime() {
@@ -37,11 +71,11 @@ function getCurrentTime() {
 	var year = currentTime.getFullYear();
 	var hours = currentTime.getHours();
 	var minutes = currentTime.getMinutes();
-	var currentTimeString = year + "/" + month + "/" + day + " ";
+	var currentTimeString = year + "/" + month + "/" + day + " " + hours + ":";
 	if (minutes < 10){
 		minutes = "0" + minutes;
 	}
-	currentTimeString += hours + ":" + minutes + " ";
+	currentTimeString += minutes + " ";
 	if (hours > 11){
 		currentTimeString += "PM";
 	} else {
@@ -51,6 +85,6 @@ function getCurrentTime() {
 }
 
 text = document.getElementById("text");
-text.onkeyup = countWords;
+text.onkeydown = countWords;
 submit = document.getElementById("submit");
 submit.onclick = tweet;
