12c684f6b25eddd2b4eed5ad42f6e7b5d3847349
zhouxinan <zhouxinan@qq.com>
2015-05-01 22:27:13 +0800

    implemented login.html

--- Project1/js/login.js
+++ Project1/js/login.js
@@ -3,7 +3,7 @@ function login() {
 		return;
 	}
 	if (validatePassword(document.getElementById("loginPassword").value)) {
-		document.getElementById("errorMessage").textContent = "";
+		showErrorMessage("");
 		document.getElementById("loginForm").submit();
 		window.location.href = "question.html";
 	}
@@ -17,7 +17,7 @@ function register() {
 		return;
 	}
 	if (validatePassword(document.getElementById("regPassword").value)) {
-		document.getElementById("errorMessage").textContent = "";
+		showErrorMessage("");
 		document.getElementById("regForm").submit();
 		window.location.href = "question.html";
 	}
@@ -26,11 +26,11 @@ function register() {
 function validateUsername(username) {
 	var usernameLength = username.length;
 	if (usernameLength == 0) {
-		document.getElementById("errorMessage").textContent = "请输入用户名！";
+		showErrorMessage("请输入用户名！");
 		return false;
 	}
-	if (usernameLength < 2 | usernameLength > 16) {
-		document.getElementById("errorMessage").textContent = "用户名长度必须是2～16个字符！";
+	if (usernameLength < 2 || usernameLength > 16) {
+		showErrorMessage("用户名长度必须是2～16个字符！");
 		return false;
 	}
 	return true;
@@ -38,14 +38,14 @@ function validateUsername(username) {
 
 function validateEmail(emailAddress) {
 	if (emailAddress.length == 0) {
-		document.getElementById("errorMessage").textContent = "请输入邮箱！";
+		showErrorMessage("请输入邮箱！");
 		return false;
 	}
 	var filter = /^[a-zA-Z0-9]+@([a-zA-Z0-9]+\.)+([a-zA-Z0-9]+)$/;
 	if (filter.test(emailAddress)) {
 		return true;
 	} else {
-		document.getElementById("errorMessage").textContent = "邮箱格式错误！";
+		showErrorMessage("邮箱格式错误！");
 		return false;
 	}
 }
@@ -53,39 +53,43 @@ function validateEmail(emailAddress) {
 function validatePassword(password) {
 	var passwordLength = password.length;
 	if (passwordLength == 0) {
-		document.getElementById("errorMessage").textContent = "请输入密码！";
+		showErrorMessage("请输入密码！");
 		return false;
 	}
-	if (passwordLength < 6 | passwordLength > 16) {
-		document.getElementById("errorMessage").textContent = "密码长度必须是6～16个字符！";
+	if (passwordLength < 6 || passwordLength > 16) {
+		showErrorMessage("密码长度必须是6～16个字符！");
 		return false;
 	}
 	var allNumbersFilter = /^\d+$/;
 	if (allNumbersFilter.test(password)) {
-		document.getElementById("errorMessage").textContent = "密码不允许是纯数字！";
+		showErrorMessage("密码不允许是纯数字！");
 		return false;
 	}
 	var numberAndLetterFilter = /^[A-Za-z0-9]+$/;
 	if (numberAndLetterFilter.test(password)) {
 		return true;
 	} else {
-		document.getElementById("errorMessage").textContent = "密码只能包含数字和字母！";
+		showErrorMessage("密码只能包含数字和字母！");
 		return false;
 	}
 }
 
-function changeForm() {
+function switchForm() {
 	if (document.getElementById("loginDiv").style.display == "block") {
 		document.getElementById("loginDiv").style.display = "none";
 		document.getElementById("regDiv").style.display = "block";
-		document.getElementById("changeFormButtonText").textContent = "登陆";
+		document.getElementById("switchFormControllerText").textContent = "登陆";
 		document.getElementById("submitButton").textContent = "注册";
 		document.getElementById("submitButton").onclick = register;
 	} else {
 		document.getElementById("loginDiv").style.display = "block";
 		document.getElementById("regDiv").style.display = "none";
-		document.getElementById("changeFormButtonText").textContent = "注册";
+		document.getElementById("switchFormControllerText").textContent = "注册";
 		document.getElementById("submitButton").textContent = "登陆";
 		document.getElementById("submitButton").onclick = login;
 	}
+}
+
+function showErrorMessage(errorMessage) {
+	document.getElementById("errorMessage").textContent = errorMessage;
 }
---
+++ Project1/js/profile.js
@@ -0,0 +1,14 @@
+$(function setTabAction() {
+	var $tab = $(".tab");
+	var $tabPane = $(".tabPane");
+	$tab.click(function(){
+		for (var i = 0; i < $tab.length; i++) {
+			if (this == $tab[i]) {
+				$tab.attr("class", "tab");
+				$(this).attr("class", "tab active");
+				$tabPane.attr("class", "tabPane");
+				$tabPane.eq(i).attr("class", "tabPane active");
+			}
+		}
+	});
+});
