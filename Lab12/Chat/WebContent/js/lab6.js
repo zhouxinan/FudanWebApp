12f740a5fea308635d23c2d6c349171306226b86
zhouxinan <zhouxinan@qq.com>
2015-06-13 12:30:35 +0800

    finished Lab12

--- Lab12/Chat/WebContent/js/lab6.js
+++ Lab12/Chat/WebContent/js/lab6.js
@@ -1,81 +1,95 @@
-
-window.onload = function(){
+window.onload = function() {
 	var display = document.getElementById("note");
-	display.style.height = window.innerHeight - 230 +"px";
+	display.style.height = window.innerHeight - 230 + "px";
 
 	var ds = document.getElementById("display");
-	ds.style.height = (window.innerHeight - 230 - 75)+"px";
+	ds.style.height = (window.innerHeight - 230 - 75) + "px";
 	ds.scrollTop = ds.scrollHeight;
 
 	var write = document.getElementById("write");
-	write.style.left = (window.innerWidth - 900)/2 + "px";
+	write.style.left = (window.innerWidth - 900) / 2 + "px";
 
 	init();
 };
 
-window.onresize = function(){
+window.onresize = function() {
 	var ds = document.getElementById("display");
-	ds.style.height = (window.innerHeight - 230 - 75)+"px";
+	ds.style.height = (window.innerHeight - 230 - 75) + "px";
 
 	var display = document.getElementById("note");
-	display.style.height = window.innerHeight - 230 +"px";
+	display.style.height = window.innerHeight - 230 + "px";
 
 	var write = document.getElementById("write");
-	write.style.left = (window.innerWidth - 900)/2 + "px";
+	write.style.left = (window.innerWidth - 900) / 2 + "px";
 };
 
-window.onunload = function(){
-	if(ws != null){
+window.onunload = function() {
+	if (ws != null) {
 		ws.close();
 	}
 }
 
-var ws ;
+var ws;
 var username;
-function init(){
+function init() {
 	username = window.sessionStorage.getItem("username");
 
-	//websocket 初始化
-	if(window.WebSocket){
-		//建立websock连接
-		ws = new WebSocket(encodeURI('ws://localhost:8080/Chat/websocket/'+encodeURIComponent(username)));
+	// websocket 初始化
+	if (window.WebSocket) {
+		// 建立websock连接
+		ws = new WebSocket(encodeURI('ws://localhost:8080/Chat/websocket/'
+				+ encodeURIComponent(username)));
 
-
-		ws.onopen = function(){
+		ws.onopen = function() {
 			console.log('socket start!!');
 		}
 
-		ws.onclose = function(){
+		ws.onclose = function() {
 			console.log('socket.end');
 		}
 
-		ws.onerror = function(){
+		ws.onerror = function() {
 			console.log('error!!');
 		}
 
-		ws.onmessage = function(event){
-			var message = JSON.parse(event.data); ;
+		ws.onmessage = function(event) {
+			var message = JSON.parse(event.data);
 			var type = message.type;
 			var itemHtml;
-			if(type == 'news'){
+			if (type == 'news') {
 				itemHtml = produceNewsHtml(message.content);
-			}else if(type == 'message'){
-				itemHtml = produceMessageHtml(message.username, message.content, message.time);
+			} else if (type == 'message') {
+				itemHtml = produceMessageHtml(message.username,
+						message.content, message.time);
 			}
 			var display = document.getElementById("display");
 			display.innerHTML = display.innerHTML + itemHtml;
 			display.scrollTop = display.scrollHeight;
 		}
 	}
-	
-	//设置文件上传的按钮
+
+	// 设置文件上传的按钮
 	$('#photo').on('click', function() {
-	    $('#file').trigger('click');
+		$('#file').trigger('click');
+	});
+	
+	setFileTypeValidator();
+}
+
+function setFileTypeValidator() {
+	$("#file").on('change', function() {
+		var file = document.getElementById("file").files[0];
+		if (file.type.match('image.*')) {
+			alert("You have successfully selected an image file!");
+		} else {
+			alert("You can only choose an image file! Please try again.");
+			resetFileInput();
+		}
 	});
 }
 
-//发送文本
-function sendMessage(content){
+// 发送文本
+function sendMessage(content) {
 	var time = new Date().toLocaleString();
 	var message = {
 		'type' : 'message',
@@ -83,15 +97,35 @@ function sendMessage(content){
 		'time' : time
 	};
 	ws.send(JSON.stringify(message));
+	var file = document.getElementById("file").files[0];
+	if (file != undefined) {
+		var fileUploadMessage = {
+			type : 'file',
+			fileName : file.name
+		};
+		ws.send(JSON.stringify(fileUploadMessage));
+		var reader = new FileReader();
+		var rawData = new ArrayBuffer();
+		reader.onload = function(e) {
+			rawData = e.target.result;
+			ws.send(rawData);
+			ws.send("end");
+		}
+		reader.readAsArrayBuffer(file);
+	} else {
+		ws.send("end");
+	}
+	resetFileInput();
 }
 
-//接受服务器文本并展示
-function produceMessageHtml(username, content, date){
+// 接受服务器文本并展示
+function produceMessageHtml(username, content, date) {
 	var ret = "<div class='noteItem'> ";
 	ret += "<img class='user-photo' src='img/user.jpg'/> ";
-	ret += "<div class='blog-content'> <span class='blog-name'>"+username+"</span> : ";
-	ret += "<span class='text-content' >" + content +"</span>";
-	ret += "<div class='info'><span class='time'>"+date+"</span>";
+	ret += "<div class='blog-content'> <span class='blog-name'>" + username
+			+ "</span> : ";
+	ret += "<span class='text-content' >" + content + "</span>";
+	ret += "<div class='info'><span class='time'>" + date + "</span>";
 	ret += "<span class='agent'>来自网页</span>";
 	ret += "<span class='operation'>转发</span></div>";
 	ret += "</div>";
@@ -99,46 +133,53 @@ function produceMessageHtml(username, content, date){
 	return ret;
 }
 
-//接受用户上线或者下线信息并展示
-function produceNewsHtml( content){
+// 接受用户上线或者下线信息并展示
+function produceNewsHtml(content) {
 	var ret = "<div class='newsItem'> ";
 	ret += "<div class='centerNews'> " + content + "</div></div>";
 	return ret;
 }
 
-document.getElementById("submit").onclick = function(){
+document.getElementById("submit").onclick = function() {
 	var promote = document.getElementById("promote").innerHTML;
-	if(Number(promote) < 0){
-		return ;
+	if (Number(promote) < 0) {
+		return;
 	}
 	var text = document.getElementById("text");
 	var content = text.value;
 	text.value = "";
-	content = content.replace(/\(F_(\d+).gif\)/g, '<img src="./img/faces/F_$1.gif">');  //正则表达式替换
+	content = content.replace(/\(F_(\d+).gif\)/g,
+			'<img src="./img/faces/F_$1.gif">'); // 正则表达式替换
 	document.getElementById("promote").innerHTML = 140;
-	if(content.length > 0){
+	if (content.length > 0) {
 		sendMessage(content);
 	}
 };
 
-/** ------------- 以下全部为lab6所实现，大家无需关心---------------------------------*/
-document.getElementById("text").oninput=function(){
+function resetFileInput() {
+	document.getElementById("reset").innerHTML = '<input type="file" class="hiden" id="file">';
+	setFileTypeValidator();
+}
+
+/** ------------- 以下全部为lab6所实现，大家无需关心--------------------------------- */
+document.getElementById("text").oninput = function() {
 	var text = document.getElementById("text");
-	var remain = 140-text.value.length;
+	var remain = 140 - text.value.length;
 	document.getElementById("promote").innerHTML = remain;
 }
 
-document.getElementById("face").onclick = function(){
+document.getElementById("face").onclick = function() {
 	var faceHtml = "<table>";
-	for(var  i=0; i<4; i++){
+	for (var i = 0; i < 4; i++) {
 		faceHtml += "<tr>";
-		for(var j=0; j<10; j++){
-			var id = i*10+j+1;
-			if(id == 40){
+		for (var j = 0; j < 10; j++) {
+			var id = i * 10 + j + 1;
+			if (id == 40) {
 				faceHtml += "<td>&nbsp;</td>"
 				continue;
 			}
-			faceHtml += "<td id='F_"+id+".gif' background='img/faces/F_"+id+".gif' onclick='chooseFace(this.id)'>&nbsp;</td>";
+			faceHtml += "<td id='F_" + id + ".gif' background='img/faces/F_"
+					+ id + ".gif' onclick='chooseFace(this.id)'>&nbsp;</td>";
 		}
 		faceHtml += "</tr>";
 	}
@@ -152,15 +193,15 @@ document.getElementById("face").onclick = function(){
 	faceDiv.style.background = "#eee";
 }
 
-function chooseFace(id){
+function chooseFace(id) {
 	faceDiv.style.display = "none";
 	var text = document.getElementById("text");
-	text.value = text.value + "("+id+")";
+	text.value = text.value + "(" + id + ")";
 	var remain = 140 - text.value.length;
 	document.getElementById("promote").innerHTML = remain;
 }
 
-function deleteMethod(id){
+function deleteMethod(id) {
 	document.getElementById("messageID").value = id;
 	document.getElementById("deleteMessage").submit();
 }
--- Lab12/Chat/src/websocket/UserSocket.java
+++ Lab12/Chat/src/websocket/UserSocket.java
@@ -1,6 +1,10 @@
 package websocket;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
@@ -16,48 +20,74 @@ import bean.Message;
 @ServerEndpoint(value = "/websocket/{user}")
 public class UserSocket {
 	private String username;
-	
+	private Message currentMessage;
+	private String fileName = null;
+	private FileOutputStream fos = null;
+	final String filePath = this.getClass().getClassLoader().getResource("")
+			.getPath()
+			+ "../../img/download/";
+
 	@OnOpen
-	public void open(Session session,  @PathParam(value = "user")String user){
+	public void open(Session session, @PathParam(value = "user") String user) {
 		try {
-			username =  java.net.URLDecoder.decode(user , "UTF-8");
+			username = java.net.URLDecoder.decode(user, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		};
-		
+		}
+
 		SocketSessionPool pool = SocketSessionPool.getPoolInstance();
 		JSONObject result = new JSONObject();
 		result.append("username", username);
 		result.append("type", "news");
-		result.append("content", "欢迎"+username+"加入！！");
+		result.append("content", "欢迎" + username + "加入！！");
 		pool.broadcast(result.toString());
 		pool.addSession(session, username);
 
 	}
-	
+
 	@OnMessage
-	public void message(String message){
-		System.out.println("Message: "+message);
-		if(!message.startsWith("{")){
+	public void message(String message) throws IOException {
+		System.out.println("Message: " + message);
+		if (!message.startsWith("{")) {
+			if (message.startsWith("end")) {
+				if (fos != null) {
+					fos.flush();
+					fos.close();
+				}
+				SocketSessionPool userPool = SocketSessionPool
+						.getPoolInstance();
+				userPool.broadcast(tranportToPublish(currentMessage));
+				currentMessage = null;
+				fileName = null;
+			}
 			return;
 		}
-		Message receivedMessage = this.transportMessageIn(message.toString());
-		String type = receivedMessage.getType();
-		SocketSessionPool userPool = SocketSessionPool.getPoolInstance();
-		if(type.equals("message")){
-			userPool.broadcast(this.tranportToPublish(receivedMessage));
-		}else{
-			System.out.println("other: "+type);
+		JSONObject messageObj = new JSONObject(message);
+		String type = messageObj.getString("type");
+		if (type.equals("message")) {
+			currentMessage = transportMessageIn(message.toString());
+		} else if (type.equals("file")) {
+			prepareFileIO(messageObj);
+		} else {
+			System.out.println("other: " + type);
 		}
 	}
-	
-	public Message transportMessageIn(String message){
+
+	@OnMessage
+	public void processUpload(ByteBuffer msg, boolean last, Session session)
+			throws IOException {
+		while (msg.hasRemaining()) {
+			fos.write(msg.get());
+		}
+	}
+
+	public Message transportMessageIn(String message) {
 		JSONObject JSONMessage = new JSONObject(message.toString());
 		String type = JSONMessage.getString("type");
 		String content = JSONMessage.getString("content");
 		String time = JSONMessage.getString("time");
-		
+
 		Message transportMessage = new Message();
 		transportMessage.setFrom(username);
 		transportMessage.setContent(content);
@@ -65,24 +95,36 @@ public class UserSocket {
 		transportMessage.setType(type);
 		return transportMessage;
 	}
-	
-	private String tranportToPublish(Message message){
+
+	private String tranportToPublish(Message message) {
 		JSONObject JSONMessage = new JSONObject();
 		JSONMessage.append("username", message.getFrom());
 		JSONMessage.append("type", "message");
+		if (fileName != null) {
+			message.setContent(message.getContent()
+					+ "<br><img src=\"img/download/" + fileName + "\"/>");
+		}
 		JSONMessage.append("content", message.getContent());
 		JSONMessage.append("time", message.getTime());
 		return JSONMessage.toString();
 	}
 
-	
+	private void prepareFileIO(JSONObject messageObj) throws IOException {
+		fileName = messageObj.getString("fileName");
+		File file = new File(filePath + fileName);
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		fos = new FileOutputStream(file);
+	}
+
 	@OnClose
-	public void close(){
+	public void close() {
 		SocketSessionPool userPool = SocketSessionPool.getPoolInstance();
 		userPool.removeSession(username);
 		JSONObject result = new JSONObject();
 		result.append("type", "news");
-		result.append("content", "欢迎"+username+"离开！！");	
+		result.append("content", "欢迎" + username + "离开！！");
 		userPool.broadcast(result.toString());
 	}
 }
